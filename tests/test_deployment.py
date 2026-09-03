ea84f4f35043d3b42e3dec37e298658e50ee78db
Ujjwal Tiwari <tiwari.ujjwal10c27@gmail.com>
2026-09-03 10:38:51 +0530

    Add deployment regression coverage

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -13,6 +13,14 @@ class DeploymentTests(unittest.TestCase):
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.get_json()['status'], 'ok')
 
+    def test_runtime_security_defaults_are_configured(self):
+        app = create_app()
+        app.config['TESTING'] = True
+
+        self.assertEqual(app.config['MAX_CONTENT_LENGTH'], 16 * 1024 * 1024)
+        self.assertEqual(app.config['SESSION_COOKIE_NAME'], 'schedule_manager_session')
+        self.assertIn(app.config['PREFERRED_URL_SCHEME'], {'http', 'https'})
+
 
 if __name__ == '__main__':
     unittest.main()
