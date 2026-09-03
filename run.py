47957f96bbf8ad8c9d3abae7968908dfc37a33b1
Ujjwal Tiwari <tiwari.ujjwal10c27@gmail.com>
2026-09-03 10:38:49 +0530

    Add runtime host and port config

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 
+import os
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = os.environ.get('HOST', '127.0.0.1')
+    port = int(os.environ.get('PORT', '5000'))
+    debug = os.environ.get('FLASK_DEBUG', '1').lower() in {'1', 'true', 'yes', 'on'}
+    app.run(host=host, port=port, debug=debug)
